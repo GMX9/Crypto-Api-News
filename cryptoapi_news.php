29b639bc53afc539c745b55897129092761382b3
GMX9 <41052152+GMX9@users.noreply.github.com>
2018-12-15 01:25:01 +0000

    Major updates V1
    Version 1 was released and updated into main core

--- cryptoapi_news.php
+++ cryptoapi_news.php
@@ -12,9 +12,16 @@ class cryptoapi{
         
     }
 
-    public function last10News($coin){ // Supported coins : BTC, ETH, LTC, XRM | ALL = TO LIST ALL COINS
+    public function lastNews($coin,$count){ // Supported coins : BTC, ETH, LTC, XRM | ALL = TO LIST ALL COINS | MAX COUNT : 10
 
-         $jsonurl = $this->url."/api/".$this->api."/last10News/".$coin;
+         $jsonurl = $this->url."/api/v1/".$this->api."/lastnews/".$coin."/".$count;
+         $json = file_get_contents($jsonurl);
+         print_r($json);
+    }
+    
+    public function sentiment($coin){ // Supported coins : BTC, ETH, LTC, XRM | ALL = TO LIST ALL COINS / pow = Proof of work 
+
+         $jsonurl = $this->url."/api/v1/".$this->api."/sentiment/".$coin;
          $json = file_get_contents($jsonurl);
          print_r($json);
     }
@@ -23,4 +30,6 @@ class cryptoapi{
 $cryptoapi = new cryptoapi("free");
 
 // Retrieves 10 most recent news 
-$cryptoapi->last10News("all");
+$cryptoapi->lastNews("all",10);
+$cryptoapi->sentiment("pow");
+$cryptoapi->sentiment("all");
